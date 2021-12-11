77e3202d300e369753c3ae2b17a2b43084e7908a
Andrey Suprano <andsuprano@gmail.com>
2021-12-11 03:15:35 -0300

    Cria contexto + ajuste de rotas

---
+++ src/context/auth.tsx
@@ -0,0 +1,66 @@
+import React, { createContext } from 'react';
+import { useState } from 'react';
+
+type UserType = {
+    nome:string,
+    cpf:string,
+    email:string,
+    setor:number,
+    telefone:string
+};
+type SessionType = {
+    token:string,
+    auth:boolean
+}
+type UserContextProps = {
+    user:UserType;
+    setUser: React.Dispatch<React.SetStateAction<UserType>>;
+};
+type SessionContextProps = {
+    session:SessionType;
+    setSession: React.Dispatch<React.SetStateAction<SessionType>>;
+};
+const SESSION_DEFAULT = {
+    session:{
+        token:"",
+        auth:true
+    },
+    setSession: () => {}
+};
+const USER_DEFAULT = {
+    user:{
+        nome:"",
+        cpf:"",
+        email:"",
+        setor:0,
+        telefone:""
+    },
+    setUser:()=>{},
+};
+
+const UserContext = createContext<UserContextProps>(USER_DEFAULT);
+const SessionContext = createContext<SessionContextProps>(SESSION_DEFAULT);
+
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(USER_DEFAULT.user);
+    const [session, setSession] = useState(SESSION_DEFAULT.session);
+    return(
+        <SessionContext.Provider value={{
+            session,
+            setSession,
+        }}>
+            <UserContext.Provider value={{
+                user,
+                setUser,
+            }}>
+                {children}
+            </ UserContext.Provider>
+        </SessionContext.Provider>
+    );
+};
+
+export { AuthProvider };
+export default {
+    UserContext,
+    SessionContext
+}
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,79 +1,17 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { FontAwesome } from "@expo/vector-icons";
-import { FontAwesome5 } from "@expo/vector-icons";
 
-import Home from "../screens/Home";
-import Menu from "../screens/Menu";
-import Store from "../screens/Store";
-import Theme from "../assets/styles/Theme";
-
-const Tab = createBottomTabNavigator();
+import { useContext } from "react";
+import Context from "../context/auth";
+import TabNavigation from "./TabNavigation.routes";
+import CadastroRoutes from "./Cadastro.routes";
+import { State } from "react-native-gesture-handler";
 
 export default function Routes() {
+  const { session, setSession } = useContext(Context.SessionContext);
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={() => ({
-          tabBarActiveTintColor: Theme.primary,
-          tabBarInactiveTintColor: Theme.title,
-          tabBarLabelStyle: {
-            fontSize: 12
-          },
-          tabBarStyle: {
-            height: '10%',
-            paddingTop: 5,
-          },
-
-        })}
-      >
-        <Tab.Screen
-          name="Home"
-          component={Home}
-          options={() => ({
-            tabBarIcon: ({ color }) => (
-              <FontAwesome5 name="home" size={26} color={color} />
-            ),
-            headerShown: false,
-          })}
-        />
-        <Tab.Screen
-          name="Store"
-          component={Store}
-          options={() => ({
-            tabBarIcon: ({ color }) => (
-              <FontAwesome5 name="store-alt" size={26} color={color} />
-            ),
-            headerShown: false,
-          })}
-        />
-        <Tab.Screen
-          name="Bíblia"
-          component={Home}
-          options={() => ({
-            tabBarIcon: ({ color }) => (
-              <FontAwesome5 name="book" size={26} color={color} />
-            ),
-            headerShown: false,
-          })}
-        />
-        <Tab.Screen
-          name="Perfil"
-          component={Menu}
-          options={() => ({
-            tabBarIcon: ({ color }) => (
-              <FontAwesome name="user" size={26} color={color} />
-            ),
-            headerTintColor: Theme.title,
-            headerTitleStyle: {
-              fontFamily: 'Montserrat_800ExtraBold',
-              fontSize: 24
-            },
-            headerTitleAlign: "center"
-          })}
-        />
-      </Tab.Navigator>
+      {session.auth ? <CadastroRoutes /> : <TabNavigation />}
     </NavigationContainer>
   );
 }
